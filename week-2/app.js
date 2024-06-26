0d3e4fab67ff040e206ac5502c4339db887291d2
Serena <serenalin0420@gmail.com>
2024-06-26 08:42:01 +0800

    add menu interaction

--- week-2/app.js
+++ week-2/app.js
@@ -64,15 +64,19 @@ console.log(discountedPrice);
 
 function twoSum(nums, target) {
   // your code here
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = 1; j < nums.length; j++) {
+      let sum = nums[i] + nums[j];
+      if (sum === target) {
+        let ans = [i, j];
+        return ans;
+      } else {
+        return "Not found";
+      }
+    }
+  }
 }
-/*
-	For example:
-		twoSum([2, 7, 11, 15], 9);
-	Should returns:
-		[0, 1]
-	Because:
-		nums[0] + nums[1] is 9
-*/
+console.log(twoSum([2, 7, 11, 15], 9));
 
 //Assignment 4
 //1. When the user clicks on the "Welcome Message" block, change text to "Have a Good Time!".
@@ -84,6 +88,19 @@ headingChange.addEventListener("click", () => {
 });
 
 //2. Click to Show/Close Menu.
+const btnOpenMenu = document.querySelector("#btn-hamburger-menu");
+const btnCloseMenu = document.querySelector("#close-side-menu");
+const sideMenu = document.querySelector("#side-menu");
+let isMenuOpen = false;
+btnOpenMenu.addEventListener("click", () => {
+  sideMenu.style.transform = "translateX(-240px)"; //打開side-menu
+  isMenuOpen = true;
+  if (isMenuOpen === true) {
+    btnCloseMenu.addEventListener("click", () => {
+      sideMenu.style.transform = "translateX(0px)";
+    });
+  }
+});
 
 //3. Click to Show More Content Boxes. 作法一
 const btnCreateContent = document.querySelector(".content-btn");
